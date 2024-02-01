3d24960d4fb6870b7c6bab24ce2cb891d29a6e2e
Arnas <arnas.e@gmail.com>
2024-02-01 16:58:29 +0200

    changed db connection credentials

--- models/index.js
+++ models/index.js
@@ -1,34 +1,51 @@
-'use strict';
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const WorkoutExercises = require('./WorkoutExercises');
-const Users = require('./Users')
+"use strict";
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const WorkoutExercises = require("./WorkoutExercises");
+const Users = require("./Users");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const env = process.env.NODE_ENV || "development";
 
+require("dotenv").config();
+
+const config = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT,
+  // ... other configurations
+};
+const db = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -48,16 +65,16 @@ db.Exercises.belongsToMany(db.Workouts, {
   unique: false,
 }); */
 
-
-
 //dummy data
 //addData(1,1);
 //addData(1,1);
-async function addData(exerciseId, workoutId){
+async function addData(exerciseId, workoutId) {
   //const testExercise = await sequelize.models.Exercises.findByPk(exerciseId)
   //const testWorkout = await sequelize.models.Workouts.findByPk(workoutId)
-  await db.WorkoutExercises.create({exerciseId: exerciseId, workoutId: workoutId});
+  await db.WorkoutExercises.create({
+    exerciseId: exerciseId,
+    workoutId: workoutId,
+  });
 }
-  
-module.exports = db;
 
+module.exports = db;
